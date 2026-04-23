29f9f83a2f77c9268547988fc8aad6c985cd87de
juanbernal13 <ja.bernalg1@uniandes.edu.co>
2026-04-23 00:52:02 -0500

    Hotfix

--- app/api/cartica-lindi/route.js
+++ app/api/cartica-lindi/route.js
@@ -4,12 +4,12 @@ const MESSAGE = {
   title: "Cartica Lindi",
   body: [
     "Amor, quiero expresarte mediante este mensaje que tú eres lo más bonito de mi mundo.",
-    "Sé que no estás pasando por un buen momento emocionalmente, y quiero que sepas que no tienes que aparentar que todo está bien.",
+    "Sé que no estás pasando por un buen momento emocionalmente, y quiero que sepas estoy aqui para ti.",
     "Puedes sentir todo lo que necesites sentir. Aquí estamos los dos, siempre para salir adelante, los dos juntitos.",
     "Entiendo todo lo que estás lidiando emocionalmente y todo lo que esto te puede estar afectando en este momento.",
     "No estás solita, amor, aquí estoy, siempre voy a estar contigo. Somos dos en esto.",
     "Siempre estaremos cogidos de la mano, siempre. Cuando no te sientas muy bien, vamos a estar los dos siempre.",
-    "Créeme que sí, te adoro con todo mi ser, amor. Eres quien da sentido a mi vida.",
+    "Créeme que sí, te amo con todo mi ser, amor. Eres quien da sentido a mi vida.",
   ],
   image: "/imagen_mensaje.png",
 };
